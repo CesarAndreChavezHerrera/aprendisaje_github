b3750e6677f53370efdc2d692ed27ba40f75e3d5
Cesar Chavez <65314179+CesarAndreChavezHerrera@users.noreply.github.com>
2020-05-13 14:55:57 -0700

    añadí un titulo
    solo por que puedo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #test de git con archivo generico de python
 
-
+print("calculadora 5000")
 print("bienvenido a la calculadora ")
 print("ingrese la operacion que quiere realizar")
 print("[1]suma, [2]resta,[3]multiplicacion, [4]divicion")
