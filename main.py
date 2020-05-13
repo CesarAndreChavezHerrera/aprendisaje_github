f3550f7f78ee28eafde240bfe845d3d2c5f3e886
Cesar Chavez <cesar1chavez2f@gmail.com>
2020-05-13 13:29:43 -0700

    leer numeros a operar

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ print("ingrese la operacion que quiere realizar")
 print("[1]suma, [2]resta,[3]multiplicacion, [4]divicion")
 opcion = input()
 
+print("ingrese el primer valor ")
+numero_uno = input
+
+print("ingrese el segundo valor")
+numero_dos = input
